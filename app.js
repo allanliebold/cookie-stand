7d8f36b12e284cff8fdbe621a17cf3139f9d9dc7
Allan Liebold <allan.liebold@gmail.com>
2017-05-20 11:43:54 -0700

    fixed linter errors

--- app.js
+++ app.js
@@ -47,41 +47,41 @@ function cookiesPerHr(min, max, cookies) {
 
 // The main function of the app. Each store object above is used as the parameter.
 function listSales(store){
-    var list = document.createElement('ul');
+  var list = document.createElement('ul');
 // Two arrays, one used to create strings which will be inserted into the page's html
-    var listArr = [];
+  var listArr = [];
 // And cookieArr stores the numbers of cookies per hour so they can be added together for the total
-    var cookieArr = [];
+  var cookieArr = [];
 // timeArr will be joined into listArr's strings so the times for each business hour will be displayed alongside the cookies sold
-    var timeArr = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm:', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm ', '6pm: ', '7pm: ', '8pm: '];
+  var timeArr = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm:', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm ', '6pm: ', '7pm: ', '8pm: '];
 
 // A for loop that uses the cookiesPerHr function above to get a number of cookies for each business hour, then pushes each number into listArr and cookieArr.
-    for (var i=0; i < 15; i++){
-      var thisHr = Math.floor(cookiesPerHr(store.minCust, store.maxCust, store.avgCookies));
-      console.log('Cookies: ', thisHr);
+  for (var i=0; i < 15; i++){
+    var thisHr = Math.floor(cookiesPerHr(store.minCust, store.maxCust, store.avgCookies));
+    console.log('Cookies: ', thisHr);
 // listArr takes the current index from the timeArr and the current cookiesPerHr/thisHr value and concatenates those into a list item.
-      listArr.push('<li>' + timeArr[i] + thisHr + '</li>');
+    listArr.push('<li>' + timeArr[i] + thisHr + '</li>');
 // cookieArr just takes the number
-      cookieArr.push(thisHr);
-    }
+    cookieArr.push(thisHr);
+  }
 
 // This loops through cookieArr and adds each number together to get the total of all of them.
-    var totalCookies = 0;
-    for (var i=0; i < cookieArr.length; i++){
-      totalCookies = totalCookies + cookieArr[i];
-    }
+  var totalCookies = 0;
+  for (var j=0; j < cookieArr.length; j++){
+    totalCookies = totalCookies + cookieArr[j];
+  }
 
-    console.log("Total cookies: ", totalCookies);
+  console.log('Total cookies: ', totalCookies);
 // This pushes the final list item to display the total cookies for the day after all of the hours
-    listArr.push('<li> Total: ' + totalCookies + '</li>');
+  listArr.push('<li> Total: ' + totalCookies + '</li>');
 // This puts a list item at the beginning of the list with the current store name as an <h2>
-    listArr.unshift('<li><h2>'+ store.name +'</h2></li>');
+  listArr.unshift('<li><h2>'+ store.name +'</h2></li>');
 
 // Joins together all of the strings pushed into listArr to create one big string
-    var fullList = listArr.join('');
-    list.innerHTML = fullList;
+  var fullList = listArr.join('');
+  list.innerHTML = fullList;
 // Inserts the list into the html document
-    document.body.appendChild(list);
+  document.body.appendChild(list);
 }
 
 // This loops through the array of store objects to run the listSales function for all 5 of them
